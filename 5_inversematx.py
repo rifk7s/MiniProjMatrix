0e8cf1602f6562d591de48c8863cb11196f5daa8
rifkys <rifkip1337@gmail.com>
2025-01-14 10:15:49 +0800

    operasi invers matriks

--- 5_inversematx.py
+++ 5_inversematx.py
@@ -83,4 +83,5 @@ if invers is None:
 else:
     print("\nInvers matriksnya adalah:")
     for baris in invers:
-        print(" ".join(f"{val:.2f}" for val in baris))
+        print(" ".join(f"{val:.2f}" for val in baris)) 
+        
